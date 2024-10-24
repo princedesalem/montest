76c8bfe057ee5de5e28ee224ea8e2f7b11059ded
princedesalem <141629036+princedesalem@users.noreply.github.com>
2024-10-24 12:07:26 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,33 @@
+
+   var i=confirm("Do you want to carry an operation");
+   var a=parseInt(prompt("enter the first number to be added"));
+   var b=parseInt(prompt("enter the second number to be added"));
+
+      function sum(a,b){
+         sum=a+b;
+         return sum;}
+
+         var sum=sum(a,b);
+         document.write("Answer for the addition:"+sum+"<br>");
+
+         
+   var a=parseInt(prompt("enter the first number to be subtracted"));
+   var b=parseInt(prompt("enter the second number to be subtracted"));
+
+       function subtract(a,b){
+         substract=a-b;
+         return subtract;}
+
+         var subtract=subtract(a,b);
+         document.write("Answer for the subtraction:"+substract+"<br>");
+
+
+   var a=parseInt(prompt("enter the first number to be multiplied"));
+   var b=parseInt(prompt("enter the second number to be multiplied"));
+
+    function multiply(a,b){
+         multiply=a*b;
+         return multiply;}
+
+         var multiply=multiply(a,b);
+         document.write("Answer for the multiplication:"+multiply+"<br>");
